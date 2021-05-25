121abcf96b3a7eb8806716d60f676e43b65bd7dc
Lucas Rigo <183015@upf.br>
2021-05-24 21:40:19 -0300

    24-05-21

--- Cadastro.php
+++ Cadastro.php
@@ -12,9 +12,20 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/c2732b0761.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="Estilos.css">
 </head>
 <body>
+
+    <div id="menu">
+		<ul>
+			<li><a href="Cadastro.php"><i class="fas fa-book"></i> Cadastrar</a></li>
+			<li><a href="Listagem.php"><i class="fas fa-list"></i> Listagem</a></li>
+		</ul>
+	</div>
+
     <div class="container">
+        <br>
         <h1><b>Cadastro de Produtos</b></h1>
         <h3>Preencha os dados do produto que deseja cadastrar</h3>
         <?php
